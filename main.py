458d8253fc0fe0b56f75229fd393247044d39909
Kai Chen <kaichen120@gmail.com>
2018-08-20 13:27:37 -0700

    Add date to tweet to avoid spam filter.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import configparser
 import os
 import sys
 import twitter
+from datetime import datetime
 
 
 class TweetRc(object):
@@ -44,16 +45,23 @@ def tweet(dialog_number):
     """
     config = TweetRc()
 
-    api = twitter.Api(consumer_key=config.get_consumer_key(), consumer_secret=config.get_consumer_secret(),
-                      access_token_key=config.get_access_key(), access_token_secret=config.get_access_secret(),
+    consumer_key = os.environ.get('consumer_key', None) or config.get_consumer_key()
+    consumer_secret = os.environ.get('consumer_secret', None) or config.get_consumer_secret()
+    access_token_key = os.environ.get('access_token', None) or config.get_access_key()
+    access_token_secret = os.environ.get('access_secret', None) or config.get_access_secret()
+
+    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
+                      access_token_key=access_token_key, access_token_secret=access_token_secret,
                       input_encoding="utf-8")
 
     dialogs = ["@RockstarGames Hey. It's Paige Harris. We worked together on that job, remember? With you know who... I basically ran the thing while he took credit. I've got a new sideline you might be interested in.. high end scores, taken elegantly, using the latest tech. ",
                "There's a Terrorbyte truck on Warstock that I can turn into our Nerve Center. Get one, we'll store it under the club, and get moving on this immedidately.",
                "@RockstarGames Hey. Paige Harris here. You and me should do this. It makes sense. Buy the Terrorbyte truck on Warstock, we turn it into a Nerve Center, and start taking scores. I've got some great ideas, I just need someone to execute them."]
 
+    current_time = datetime.now().strftime('%H:%M:%S')
+
     try:
-        status = api.PostUpdate(dialogs[dialog_number] + "#4")
+        status = api.PostUpdate(dialogs[dialog_number] + current_time)
     except UnicodeDecodeError:
         print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
         sys.exit(2)
