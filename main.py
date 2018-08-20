469c441d1e24ca9338c0c0dc82a4feef822b472e
Kai Chen <kaichen120@gmail.com>
2018-08-20 14:56:48 -0700

    Removed need for url param. Tweet is based on hour.

--- main.py
+++ main.py
@@ -35,12 +35,32 @@ class TweetRc(object):
         return self._config
 
 
-def tweet(dialog_number):
+def prepare_post_dialog():
+    """
+    Prepares the post dialog.
+
+    """
+    dialogs_paige = [
+        "@RockstarGames Hey. It's Paige Harris. We worked together on that job, remember? With you know who... I basically ran the thing while he took credit. I've got a new sideline you might be interested in.. high end scores, taken elegantly, using the latest tech. ",
+        "@RockstarGames Hey. Paige Harris here. You and me should do this. It makes sense. Buy the Terrorbyte truck on Warstock, we turn it into a Nerve Center, and start taking scores. I've got some great ideas, I just need someone to execute them."]
+
+    current_time = datetime.now().strftime('%H:%M:%S')
+    current_time_hour = int(datetime.now().strftime("%H"))
+
+    # If current time is even, post reply index 0
+    if current_time_hour % 2 == 0:
+        post_dialog = dialogs_paige[0] + current_time
+
+    # else, current time is odd. post index 1 reply
+    else:
+        post_dialog = dialogs_paige[1] + current_time
+
+    return post_dialog
+
+
+def tweet():
     """
     Uses the python twitter library to tweet from an array of possible dialogs.
-
-    Args:
-        dialog_number: Corresponds to the index on the dialogs array to post.
 
     """
     config = TweetRc()
@@ -54,14 +74,10 @@ def tweet(dialog_number):
                       access_token_key=access_token_key, access_token_secret=access_token_secret,
                       input_encoding="utf-8")
 
-    dialogs = ["@RockstarGames Hey. It's Paige Harris. We worked together on that job, remember? With you know who... I basically ran the thing while he took credit. I've got a new sideline you might be interested in.. high end scores, taken elegantly, using the latest tech. ",
-               "There's a Terrorbyte truck on Warstock that I can turn into our Nerve Center. Get one, we'll store it under the club, and get moving on this immedidately.",
-               "@RockstarGames Hey. Paige Harris here. You and me should do this. It makes sense. Buy the Terrorbyte truck on Warstock, we turn it into a Nerve Center, and start taking scores. I've got some great ideas, I just need someone to execute them."]
-
-    current_time = datetime.now().strftime('%H:%M:%S')
+    post_dialog = prepare_post_dialog()
 
     try:
-        status = api.PostUpdate(dialogs[dialog_number] + current_time)
+        status = api.PostUpdate(post_dialog)
     except UnicodeDecodeError:
         print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
         sys.exit(2)
--- server.py
+++ server.py
@@ -10,11 +10,12 @@ def index():
 
 @app.route('/tweet')
 def tweet_route():
-    """The endpoint for a webhook of sorts that should trigger a post.
-    The request MUST have a dialog_number parameter which references which dialog it would like to post."""
+    """
+    The endpoint for a webhook of sorts that should trigger a post.
 
+    """
     try:
-        tweet(int(request.args.get('dialog_number')))
+        tweet()
     except Exception as e:
         # TODO: Fix this up. Better error handling.
         print(e)
